31347c8e9203c240d07b5ed4837476dea66df7fd
zhangpeihang <948869991@qq.com>
2022-04-12 20:28:29 +0800

    feat: 百度文字转语音

---
+++ src/Extensions/Components/BootstrapBlazor.BaiduSpeech/Services/BaiduSynthesizerProvider.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+internal class BaiduSynthesizerProvider : ISynthesizerProvider, IAsyncDisposable
+{
+    [NotNull]
+    public string? Name { get; set; } = "Azure";
+
+    private DotNetObjectReference<BaiduSynthesizerProvider>? Interop { get; set; }
+
+    private IJSObjectReference? Module { get; set; }
+
+    [NotNull]
+    private SynthesizerOption? Option { get; set; }
+
+    private BaiduSpeechOption SpeechOption { get; }
+
+    private IJSRuntime JSRuntime { get; }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="options"></param>
+    /// <param name="runtime"></param>
+    public BaiduSynthesizerProvider(IOptions<BaiduSpeechOption> options, IJSRuntime runtime)
+    {
+        JSRuntime = runtime;
+        SpeechOption = options.Value;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="option"></param>
+    /// <returns></returns>
+    public async Task InvokeAsync(SynthesizerOption option)
+    {
+        if (!string.IsNullOrEmpty(option.Text))
+        {
+            if (string.IsNullOrEmpty(option.MethodName))
+            {
+                throw new InvalidOperationException();
+            }
+
+            Option = option;
+            if (Module == null)
+            {
+                Module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.AzureSpeech/js/synthesizer.js");
+            }
+            Interop ??= DotNetObjectReference.Create(this);
+            await Module.InvokeVoidAsync(Option.MethodName, Interop, nameof(Callback), Option.Text);
+        }
+        else
+        {
+            if (option.Callback != null)
+            {
+                await option.Callback(SynthesizerStatus.Close);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="status"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task Callback(SynthesizerStatus status)
+    {
+        if (Option.Callback != null)
+        {
+            await Option.Callback(status);
+        }
+    }
+
+    /// <summary>
+    /// DisposeAsync 方法
+    /// </summary>
+    /// <param name="disposing"></param>
+    private async ValueTask DisposeAsync(bool disposing)
+    {
+        if (disposing)
+        {
+            if (Interop != null)
+            {
+                Interop.Dispose();
+            }
+            if (Module is not null)
+            {
+                await Module.DisposeAsync();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsync(true);
+        GC.SuppressFinalize(this);
+    }
+}
