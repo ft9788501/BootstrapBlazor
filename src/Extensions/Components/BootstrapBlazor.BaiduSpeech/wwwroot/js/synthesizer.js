be6a24129907bee50108773c19ab313c2b3d9b83
Argo-Tianyi <argo@163.com>
2022-04-14 02:55:23 +0800

    refactor: 移除 audio 标签呢

--- src/Extensions/Components/BootstrapBlazor.BaiduSpeech/wwwroot/js/synthesizer.js
+++ src/Extensions/Components/BootstrapBlazor.BaiduSpeech/wwwroot/js/synthesizer.js
@@ -3,14 +3,11 @@
 export function bb_baidu_speech_synthesizerOnce(obj, callback, data) {
     audio = document.createElement("audio");
     audio.controls = true;
-    audio.style.display = "none";
-    document.body.appendChild(audio);
 
     var blob = new Blob([data], { type: 'audio/mp3' });
     var url = (window.URL || webkitURL).createObjectURL(blob);
     audio.src = url;
     audio.addEventListener("ended", function () {
-        document.body.removeChild(audio);
         audio = undefined;
         obj.invokeMethodAsync(callback, "Finished");
     });
@@ -21,7 +18,6 @@ export function bb_baidu_speech_synthesizerOnce(obj, callback, data) {
 export function bb_baidu_close_synthesizer(obj, callback) {
     if (audio != undefined) {
         audio.pause();
-        document.body.removeChild(audio);
     }
     obj.invokeMethodAsync(callback, "Finished");
 };
