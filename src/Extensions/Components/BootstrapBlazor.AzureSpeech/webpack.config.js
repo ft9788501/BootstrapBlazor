de86c95a57960ab2c6fb9671ec3b38ab114c9a52
zhangpeihang <948869991@qq.com>
2022-04-13 11:27:49 +0800

    feat: 增加 webpack 打包
    # Conflicts:
    #	src/BootstrapBlazor.Server/appsettings.json
    #	src/Extensions/Components/BootstrapBlazor.AzureSpeech/Services/AzureRecognizerProvider.cs
    #	src/Extensions/Components/BootstrapBlazor.AzureSpeech/wwwroot/js/recognizer.js

---
+++ src/Extensions/Components/BootstrapBlazor.AzureSpeech/webpack.config.js
@@ -0,0 +1,27 @@
+﻿const path = require("path");
+const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: "./wwwroot/js/speech.js",
+    output: {
+        path: path.resolve(__dirname, "wwwroot/js"),
+        filename: "microsoft-cognitiveservices-speech-sdk-bundle.js",
+        library: "Speech",
+        libraryTarget: "var"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                },
+                exclude: '/node_modules/',
+            }
+        ]
+    },
+    plugins: [
+        new EsmWebpackPlugin()
+    ]
+}
--- src/Extensions/Components/BootstrapBlazor.AzureSpeech/wwwroot/js/recognizer.js
+++ src/Extensions/Components/BootstrapBlazor.AzureSpeech/wwwroot/js/recognizer.js
@@ -1,27 +1,35 @@
-﻿var recognizer = undefined;
+﻿import { SpeechTranslationConfig, AudioConfig, TranslationRecognizer } from 'microsoft-cognitiveservices-speech-sdk'
+
+var recognizer = null;
+
+export function bb_close(obj, method) {
+    if (recognizer != null) {
+        recognizer.close();
+    }
+    obj.invokeMethodAsync(method, '');
+}
 
 export function bb_speech_recognizeOnce(obj, method, token, region, recognitionLanguage, targetLanguage) {
-    var SpeechSDK = window.SpeechSDK;
-    var speechConfig = SpeechSDK.SpeechTranslationConfig.fromAuthorizationToken(token, region);
+
+    var speechConfig = SpeechTranslationConfig.fromAuthorizationToken(token, region);
     speechConfig.speechRecognitionLanguage = recognitionLanguage;
     speechConfig.addTargetLanguage(targetLanguage)
 
-    var audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
-    recognizer = new SpeechSDK.TranslationRecognizer(speechConfig, audioConfig);
+    var audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+    recognizer = new TranslationRecognizer(speechConfig, audioConfig);
 
     recognizer.recognizeOnceAsync(function (successfulResult) {
         recognizer.close();
-        recognizer = undefined;
+        recognizer = null;
         obj.invokeMethodAsync(method, successfulResult.privText);
     }, function (err) {
         console.log(err);
     });
 }
 
-export function bb_close(obj, method) {
-    if (recognizer != undefined) {
-        recognizer.close();
-        recognizer = undefined;
-    }
-    obj.invokeMethodAsync(method, '');
-}
+
+
+
+
+
+
